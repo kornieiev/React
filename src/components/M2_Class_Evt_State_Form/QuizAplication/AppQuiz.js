79001db6a0c03824f93665742d7c4f1fe56b0c96
Dima Kornieiev <108156304+kornieiev@users.noreply.github.com>
2024-05-31 18:44:26 +0200

    filter

--- src/components/M2_Class_Evt_State_Form/QuizAplication/AppQuiz.js
+++ src/components/M2_Class_Evt_State_Form/QuizAplication/AppQuiz.js
@@ -10,20 +10,50 @@ export default class AppQuiz extends Component {
     quizItems: initialQuizItems,
     filters: {
       topic: '',
-      level: 'all',
+      level: '',
     },
   };
 
+  updateQuizeTopic = newTopic => {
+    console.log('newTopic', newTopic);
+    this.setState(prevState => {
+      return { filters: { ...prevState.filters, topic: newTopic } };
+    });
+  };
+
+  updateQuizeLevel = newLevel => {
+    console.log('newLevel', newLevel);
+    this.setState(prevState => {
+      return { filters: { ...prevState.filters, level: newLevel } };
+    });
+  };
+
   render() {
-    const { quizItems } = this.state;
-    console.log('quizItems', quizItems);
+    const {
+      quizItems,
+      filters: { topic, level },
+    } = this.state;
+    console.log('quizItems', quizItems[0].topic);
+
+    const visibleQuizItems = quizItems.filter(item => {
+      console.log('item', item);
+
+      return item.topic.toLowerCase().includes(topic.toLowerCase());
+    });
+    console.log('visibleQuizItems', visibleQuizItems);
 
     return (
       <div>
         <QuizForm />
-        <SearchBar />
-        {quizItems.length > 0 && <QuizList items={quizItems} />}
+        <SearchBar
+          filters={this.state.filters}
+          updateQuizeTopic={this.updateQuizeTopic}
+          updateQuizeLevel={this.updateQuizeLevel}
+        />
+        {quizItems.length > 0 && <QuizList items={visibleQuizItems} />}
       </div>
     );
   }
 }
+
+// https://youtu.be/iu1Is5W--2s?t=4400
--- src/components/M2_Class_Evt_State_Form/QuizAplication/SearchBar.jsx
+++ src/components/M2_Class_Evt_State_Form/QuizAplication/SearchBar.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
 
-export default function SearchBar() {
+export default function SearchBar({
+  filters: { topic, level },
+  updateQuizeTopic,
+  updateQuizeLevel,
+}) {
   return (
     <div>
-      <input type="text" placeholder="Topic filter" />
-      <select>
+      <input
+        type="text"
+        value={topic}
+        placeholder="Topic filter"
+        onChange={e => {
+          console.log('e:', e.target.value);
+          updateQuizeTopic(e.target.value);
+        }}
+      />
+      <select
+        value={topic}
+        onChange={e => {
+          console.log('e:', e.target.value);
+          updateQuizeLevel(e.target.value);
+        }}
+      >
         <option value="all">All</option>
         <option value="beginner">Beginner</option>
         <option value="intermediate">Intermediate</option>
